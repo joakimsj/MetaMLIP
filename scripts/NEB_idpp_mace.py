d9f99e975ef87f4a6083f15f0281ee14e74a38cb
Joakim Jestilä <88662850+joakimsj@users.noreply.github.com>
2026-02-06 14:20:18 +0200

    Update NEB_idpp_mace.py
    updated to a crude height threshold for fixing parts of NEB-images

--- scripts/NEB_idpp_mace.py
+++ scripts/NEB_idpp_mace.py
@@ -4,12 +4,13 @@ from ase import units
 from ase.constraints import FixAtoms
 from mace.calculators import mace_mp, MACECalculator
 from ase.mep import NEB
-from ase.optimize.fire import FIRE as QuasiNewton
+from ase.optimize.fire import FIRE as QuasiNewton # consider BGFS
 from sys import argv
 
 # Read initial and final geometries
 initial = read(argv[1])
 final = read(argv[2])
+z_threshold = 2.6
 initial_name = os.path.splitext(argv[1])[0]
 final_name = os.path.splitext(argv[2])[0]
 
@@ -20,6 +21,11 @@ initial_calc = MACECalculator(model_paths=models, device='cuda', enablecueq=True
 
 # Attach calculator and compute forces for initial state
 initial.calc = initial_calc
+
+fixed_indices = [i for i, atom in enumerate(initial) if atom.position[2] < z_threshold]
+fix_constraint = FixAtoms(indices=fixed_indices)
+initial.set_constraint(fix_constraint)
+
 relax = QuasiNewton(initial)
 relax.run(fmax=0.05)
 #initial_energy = initial.get_potential_energy()
@@ -29,6 +35,11 @@ final_calc = MACECalculator(model_paths=models, device='cuda', enablecueq=True, 
 
 # Attach calculator and compute forces for final state
 final.calc = final_calc
+
+fixed_indices = [i for i, atom in enumerate(final) if atom.position[2] < z_threshold]
+fix_constraint = FixAtoms(indices=fixed_indices)
+final.set_constraint(fix_constraint)
+
 relax = QuasiNewton(final)
 relax.run(fmax=0.05)
 
@@ -48,10 +59,11 @@ for image in images[1:]:
 # Append final image
 images.append(final)
 
-# Constrain substrate atoms
-#constraint = FixAtoms(mask=[atom.symbol in {'Na', 'Cl', #'Au'} for atom in initial])
-#for image in images:
-#    image.set_constraint(constraint)
+# Constrain substrate atoms of intermediate images
+fixed_indices = [i for i, atom in enumerate(image) if atom.position[2] < z_threshold]
+fix_constraint = FixAtoms(indices=fixed_indices)
+for image in images:
+    image.set_constraint(constraint)
 
 # Run IDPP interpolation
 neb = NEB(images)
